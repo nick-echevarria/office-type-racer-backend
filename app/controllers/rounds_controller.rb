f3a89370d8fca5f418bc608155820ef0d8674050
nick-echevarria <nick.echev@gmail.com>
2020-05-05 19:59:16 -0400

    Create method for Round

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -8,4 +8,22 @@ class RoundsController < ApplicationController
         round = Round.find_by(id: params[:id])
         render json: round, include: [:user, :quote], except: [:user_id, :quote_id,:created_at, :updated_at]
     end
+
+    def create
+        round = Round.create(user_id: params[:user_id], quote_id: params[:quote_id], score: params[:score], completion_time: params[:completion_time])        
+        # if @user.valid?
+        render json: round, except: [:created_at, :updated_at]
+        # else 
+        #     flash[:errors] = @user.errors.full_messages
+        #     render json: user
+        # end
+
+        # @user = User.new(_params)
+        # if @user.save
+        #     render json: user, except: [:created_at, :updated_at]
+        # else
+        #     flash[:errors] = @user.errors.full_messages
+        #     render json: user, except: [:created_at, :updated_at]
+        # end        
+    end
 end
